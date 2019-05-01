92a02a6fea6da02406e1341f51d9362fde20d7ce
ivavivanova <ivavivanova@outlook.com>
2019-05-01 14:29:38 +0300

    Added an exception handler

---
+++ src/Prestige.Kernel.ExceptionHandler/Constants/ExceptionHandlerConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Prestige.Kernel.ExceptionHandler.Constants
+{
+    public static class ExceptionHandlerConstants
+    {
+        public const string ResponseContentType = "text/html";
+
+        public const string PublicErrorMessageFormat = "<h1>Attention! An error occurred!</h1><p> Error: {0}</p><br/>";
+    }
+}
---
+++ src/Prestige.Kernel.ExceptionHandler/Extensions/ExceptionHandlerExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Prestige.Kernel.ExceptionHandler.Constants;
+using System.Net;
+
+namespace Prestige.Kernel.ExceptionHandler.Extensions
+{
+    public static class ExceptionHandlerExtensions
+    {
+        public static void AddExceptionHandlerSettings(this IApplicationBuilder app, bool isDevelopment)
+        {
+              app.UseExceptionHandler(
+                options =>
+                {
+                    options.Run(
+                    async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.ContentType = ExceptionHandlerConstants.ResponseContentType;
+                        var ex = context.Features.Get<IExceptionHandlerFeature>();
+                        if (ex != null)
+                        {
+                            var err = string.Format(ExceptionHandlerConstants.PublicErrorMessageFormat, ex.Error.Message);
+
+                            if (isDevelopment)
+                            {
+                                err += ex.Error.StackTrace;
+                            }
+
+                            await context.Response.WriteAsync(err).ConfigureAwait(false);
+                        }
+                    });
+                });
+        }
+    }
+}

d23368f230aefdc5a2d29692be9ea791dc010ac4
Miroslav Stoyanov <miro@torussoft.com>
2019-05-01 18:11:22 +0300

    Setup packages to generate on build.

--- src/Prestige.Kernel.ExceptionHandler/Extensions/ExceptionHandlerExtensions.cs
+++ src/Prestige.Kernel.ExceptionHandler/Extensions/ExceptionHandlerExtensions.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+
 using Prestige.Kernel.ExceptionHandler.Constants;
+
 using System.Net;
 
 namespace Prestige.Kernel.ExceptionHandler.Extensions
@@ -11,28 +13,28 @@ namespace Prestige.Kernel.ExceptionHandler.Extensions
     {
         public static void AddExceptionHandlerSettings(this IApplicationBuilder app, bool isDevelopment)
         {
-              app.UseExceptionHandler(
-                options =>
-                {
-                    options.Run(
-                    async context =>
-                    {
-                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                        context.Response.ContentType = ExceptionHandlerConstants.ResponseContentType;
-                        var ex = context.Features.Get<IExceptionHandlerFeature>();
-                        if (ex != null)
-                        {
-                            var err = string.Format(ExceptionHandlerConstants.PublicErrorMessageFormat, ex.Error.Message);
+            app.UseExceptionHandler(
+              options =>
+              {
+                  options.Run(
+                  async context =>
+                  {
+                      context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                      context.Response.ContentType = ExceptionHandlerConstants.ResponseContentType;
+                      IExceptionHandlerFeature ex = context.Features.Get<IExceptionHandlerFeature>();
+                      if (ex != null)
+                      {
+                          string err = string.Format(ExceptionHandlerConstants.PublicErrorMessageFormat, ex.Error.Message);
 
-                            if (isDevelopment)
-                            {
-                                err += ex.Error.StackTrace;
-                            }
+                          if (isDevelopment)
+                          {
+                              err += ex.Error.StackTrace;
+                          }
 
-                            await context.Response.WriteAsync(err).ConfigureAwait(false);
-                        }
-                    });
-                });
+                          await context.Response.WriteAsync(err).ConfigureAwait(false);
+                      }
+                  });
+              });
         }
     }
 }
